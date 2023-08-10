d84d97dead15f2c93d66c15e7220f50cf8ca45a1
ohan <hoangphuongnam17@gmail.com>
2023-08-10 11:01:39 +0700

    fix pixel perfect movement

--- characters/player/PlayerUI.gd
+++ characters/player/PlayerUI.gd
@@ -1,24 +1,25 @@
 extends MarginContainer
 
+
 @onready var health_bar = $HBoxContainer2/LifeBar
 @onready var dur_bar = $HBoxContainer2/DurabilityBar
 @onready var resource_label = $HBoxContainer/ResourceLabel
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	$"../../..".health_updated.connect(update_health)
+	Player.instance.health_updated.connect(update_health)
+	Player.instance.get_node("%WeaponHolder").durability_updated.connect(update_dur)
 	Global.resources_updated.connect(update_resource_label)
-	$"../../../WeaponHolder".durability_updated.connect(update_dur)
-	
+
+
 func update_health(perc_health : float) -> void:
 	health_bar.value = perc_health
-	
+
+
 func update_dur(perc_dur : float) -> void:
 	dur_bar.set_visible(perc_dur > 0)
 	dur_bar.value = perc_dur
-	
+
+
 func update_resource_label(val : int) -> void:
 	resource_label.text = str(val)
-
-
-

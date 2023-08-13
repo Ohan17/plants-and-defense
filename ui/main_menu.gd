f55ea42f9f4a9fcd0ce35193a2c0c21f17f34f84
Hiasae <16280810+Hiasae@users.noreply.github.com>
2023-08-13 20:17:42 +0200

    Intro sequence added
    - Added intro sequence

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,7 +1,10 @@
 extends TextureRect
 
+@onready var anim_player = $AnimationPlayer
 
 func _on_start_button_button_down() -> void:
+	anim_player.play("Intro_1")
+	await anim_player.animation_finished
 	get_tree().change_scene_to_file("res://level.tscn")
 
 

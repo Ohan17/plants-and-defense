73e414e777ff4f887594c5f6ed4db3b8cc9bd982
ohan <hoangphuongnam17@gmail.com>
2023-08-14 17:43:16 +0700

    small fix

--- misc/pixel_perfect_visual.gd
+++ misc/pixel_perfect_visual.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if abs(root_node.velocity.aspect()) == 1:
 		var delta_pos: Vector2 = root_node.global_position.round() - pre_pos
-		if abs(delta_pos) == Vector2.ONE:
+		if abs(delta_pos.aspect()) == 1:
 			global_position = root_node.global_position.round()
 		elif delta_pos[delta_pos.max_axis_index()] > 1:
 			global_position += delta_pos
